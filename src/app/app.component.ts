8d91df11c4c6c4806d8ddb0c041a6c672535b519
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-24 22:44:45 +0100

    Main Component

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+
+import { AuthService } from "./core/services/auth/auth.service";
+import { LoadingService } from "./core/services/utils/loading.service";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  isLoggedIn$: Observable<boolean>;
+
+  showLoader: boolean;
+
+  title = "app";
+
+  constructor(
+    private authService: AuthService,
+    private loadingService: LoadingService
+  ) {}
+
+  ngOnInit() {
+    this.isLoggedIn$ = this.authService.isLoggedIn;
+
+    console.log("[AppComponent] showLoader: " + this.showLoader);
+
+    this.loadingService.status.subscribe((val: boolean) => {
+      this.showLoader = val;
+    });
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
+// components
+import { HeaderComponent } from './ui-components/header/header.component';
+import { SidebarComponent } from './ui-components/sidebar/sidebar.component';
+import { FooterComponent } from './ui-components/footer/footer.component';
+import { AlertComponent } from './ui-components/alert/alert.component';
+
+
+// guards
+import { AuthGuard } from './core/guards/auth.guard'
+
+//services
+import { AlertService } from './core/services/utils/alert.service';
+import { LoadingService } from './core/services/utils/loading.service';
+
+import { AuthService } from './core/services/auth/auth.service'
+import { SectionService } from './core/services/system-management/section.service'
+import { StandardService } from './core/services/system-management/standard.service'
+import { StudentService } from './core/services/user-management/student.service'
+import { TeacherService } from './core/services/user-management/teacher.service'
+import { TransportManagerService } from './core/services/user-management/transport-manager.service'
+import { FinanceManagerService } from './core/services/user-management/finance-manager.service'
+import { PrincipalService } from './core/services/user-management/principal.service'
+import { CounsellorService } from './core/services/user-management/counsellor.service';
+import { AttendanceService } from './core/services/system-management/attendance.service'; 
+import { StationeryService } from './core/services/system-management/stationery.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    AlertComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpModule
+  ],
+  providers: [ AuthGuard, AlertService, LoadingService, AuthService, SectionService, StandardService, StudentService, TeacherService, TransportManagerService, FinanceManagerService, PrincipalService, CounsellorService, AttendanceService, StationeryService],
+
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
