2d429491127f5c655260ce600bdd176dfd76ba7e
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-25 10:24:05 +0100

    Implementing main routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,45 @@
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AuthGuard } from './core/guards/auth.guard'
+
+
+const routes: Routes = [
+  { 
+    path: 'auth', 
+    loadChildren: './auth/auth.module#AuthModule'
+  },
+  { 
+    path: '', 
+    loadChildren: './dashboard/dashboard.module#DashboardModule',
+    canActivate: [AuthGuard]
+  },
+  { 
+    path: 'dashboard', 
+    loadChildren: './dashboard/dashboard.module#DashboardModule',
+    canActivate: [AuthGuard]
+  },
+  { 
+    path: 'layouts', 
+    loadChildren: './dummy/dummy.module#DummyModule',
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'users',
+    loadChildren: './user-management/user-management.module#UserManagementModule',
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'systems',
+    loadChildren: './system-management/system-management.module#SystemManagementModule',
+    canActivate: [AuthGuard]
+  }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
