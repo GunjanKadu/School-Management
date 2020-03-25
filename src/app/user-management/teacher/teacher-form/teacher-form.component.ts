4fd3328193ba53e10bfe8fb46a7633f49a4dddac
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-25 17:17:40 +0100

    Teacher form  Testing

---
+++ src/app/user-management/teacher/teacher-form/teacher-form.component.ts
@@ -0,0 +1,114 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/filter';
+
+import { AlertService } from '../../../core/services/utils/alert.service';
+import { LoadingService } from '../../../core/services/utils/loading.service';
+import { TeacherService } from '../../../core/services/user-management/teacher.service';
+
+import { TabManager } from '../../../core/helpers/tabManager';
+
+@Component({
+  selector: 'app-teacher-form',
+  templateUrl: './teacher-form.component.html',
+  styleUrls: ['./teacher-form.component.css']
+})
+export class TeacherFormComponent extends TabManager implements OnInit {
+
+  data: any;
+  dataSource: any;
+
+  public isRole: boolean = false;
+ 
+  form: FormGroup;
+
+  private formSubmitAttempt: boolean;
+  formData: FormData = new FormData();
+
+  constructor(
+  private fb: FormBuilder,
+  private teacherService: TeacherService,
+  private alertService: AlertService,
+  private loadingService: LoadingService,
+  private route: ActivatedRoute,
+  private router: Router
+  ) { 
+    super();
+  }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+        name:  ['', Validators.required],
+        father_name:  ['', Validators.required],
+        gender:   ['', Validators.required],
+        email:  ['', Validators.required],
+        dob:  ['', Validators.required],
+        doj:  ['', Validators.required],
+        tel_no:   ['', Validators.required],
+        mob_no:   ['', Validators.required],
+        aadhar_id:   ['', Validators.required],
+        marital_status:   ['', Validators.required],
+        address:   ['', Validators.required],
+        education:   ['', Validators.required],
+        work_exp:   ['', Validators.required],
+        bank_name:   ['', Validators.required],
+        bank_acc_no:   ['', Validators.required],
+        password:   ['', Validators.required],
+      });
+
+    // calling openTab from TabManager
+    this.openTab('teacher_tab');
+
+    this.route.paramMap
+    .filter(params => params.get('id') !== undefined && params.get('id') !== null)
+    .switchMap((params: ParamMap) => this.teacherService.getById(params.get('id')))
+    .subscribe((res)=> {
+        this.loadingService.display(false);
+        console.log('Edit [TeacherFormComponent] Response =>' +JSON.stringify(res));
+        this.data = res.data;
+        this.form.patchValue({_id:this.data._id});
+        console.log('Edit [TeacherFormComponent] data  =>' +JSON.stringify(this.data)); 
+            
+        if(this.data.length == 0) {
+           this.alertService.info("No records found !!!");
+        }
+
+    },(err) => {
+          const errBody = err.json();
+          console.log('Edit [TeacherFormComponent] Error  =>' +errBody);
+    })
+  }
+
+  isFieldInvalid(field: string) {
+    return (
+      (!this.form.get(field).valid && this.form.get(field).touched) ||
+      (this.form.get(field).untouched && this.formSubmitAttempt)
+    );
+  }
+
+onSubmit() {
+    console.log('onSubmit()');
+    console.log(' onSubmit() teacher data '+JSON.stringify(this.form.value));
+
+    if (this.form.valid) {
+      this.loadingService.display(true);
+      this.teacherService.add(this.form.value).subscribe((res)=> {
+        this.loadingService.display(false);
+        console.log('[TeacherFormComponent] Response =>' +JSON.stringify(res));
+        this.formSubmitAttempt = true;
+        this.form.reset()
+        this.alertService.success("Teacher added successfully");
+        },(err) => {
+          this.loadingService.display(false);
+          const errBody = err.json();
+          console.log('[TeacherFormComponent] Error =>' +errBody);
+          this.alertService.success("[TeacherFormComponent] failed to add teacher");
+        });
+      }
+    else{
+      console.log("form data is not valid");
+    }
+  }
+}
